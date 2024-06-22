a87fc663ea8da2fb0a9c925ca38774d42b3c8dc5
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2024-06-22 23:04:31 +0300

    chore : wait to add SVM before the denoiser to detect the noise type

---
+++ svm.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# 1 read images
+# read train.csv and test.csv
+# train = pd.read_csv('train.csv')
+# test = pd.read_csv('test.csv')
+
+
+# 2 add noise and save the label as the key
+# 3 train the model
+# 4 test the model
+# 5 save the model
+# 6 after the training finish and the model is saved, we can use the model to detect the noise type and apply the correct function to remove the noise
+# 7 calculate the SSIM and PSNR for each denoised image
+
